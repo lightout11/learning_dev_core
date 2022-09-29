25a95fcc3a8f9ce307a7e219fcc11b5731e48e34
lightout <lightout01@outlook.com>
2022-09-29 09:47:51 +0700

    Add interaction

--- client/source/main.cpp
+++ client/source/main.cpp
@@ -1,11 +1,16 @@
 #include "client_socket.h"
 #include <string>
+#include <iostream>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    string ip_address = "127.0.0.1";
-    int port = 7777;
+    cout << "Enter IP address: ";
+    string ip_address;
+    cin >> ip_address;
+    cout << "Enter port: ";
+    int port;
+    cin >> port;
 
     ClientSocket client_socket(ip_address, port);
     client_socket.Communicate();
