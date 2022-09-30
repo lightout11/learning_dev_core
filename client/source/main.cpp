81d004a0154b996ffaba8f3e64775f6aaa48346e
lightout <lightout01@outlook.com>
2022-09-30 05:35:42 -0400

    change input interface

--- client/source/main.cpp
+++ client/source/main.cpp
@@ -1,11 +1,29 @@
 #include "client_socket.h"
 #include <string>
+#include <iostream>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    string ip_address = "127.0.0.1";
-    int port = 7777;
+    string default_ip_address = "127.0.0.1";
+    int default_port = 10000;
+    string ip_address = "";
+    int port = 0;
+    switch (argc) {
+        case 1:
+            cout << "Enter IP address: ";
+            cin >> ip_address;
+            cout << "Enter port: ";
+            cin >> port;
+            break;
+        case 3:
+            ip_address = argv[1];
+            port = stoi(argv[2]);
+            break;
+        default:
+            cout << "Invalid input.";
+            return 1;
+    }
 
     ClientSocket client_socket(ip_address, port);
     client_socket.Communicate();
--- server/source/main.cpp
+++ server/source/main.cpp
@@ -4,8 +4,22 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    string ip_address = "127.0.0.1";
-    int port = 7777;
+    string default_ip_address = "127.0.0.1";
+    int default_port = 10000;
+    string ip_address = "";
+    int port = 0;
+    switch (argc) {
+        case 1:
+            ip_address = default_ip_address;
+            port = default_port
+        case 3:
+            ip_address = argv[1];
+            port = stoi(argv[2]);
+            break;
+        default:
+            cout << "Invalid input.";
+            return 1;
+    }
     
     ServerSocket server_socket(ip_address, port);
     server_socket.Listen();
